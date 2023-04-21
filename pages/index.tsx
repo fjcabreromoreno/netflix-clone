2c8cfd012a3a77fce7c4187b8a0ec40f15c645ec
Francisco Javier Cabrero Moreno <fjcabreromoreno@gmail.com>
2023-04-21 12:29:15 +0200

    movies list & movie card; also movies endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
+import Billboard from '../components/Billboard';
+import MoviesList from '../components/MoviesList';
 import Navbar from '../components/Navbar';
+import useMovieList from '../hooks/uesMoviesList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,9 +23,13 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
+      <Billboard />
+      <MoviesList title="Trending Now" data={movies} />
     </>
   );
 }
